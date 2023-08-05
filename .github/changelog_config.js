3dd69520f4e482fccddd7a3915804c4f2d1d176e
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-08-05 11:53:17 +0200

    Add automated release notes

---
+++ .github/changelog_config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    types: [
+        { types: ['feature'], label: 'Features 🎉' },
+        { types: ['fix'], label: 'Bug Fixes 🐛' },
+        { types: ['perf'], label: 'Performance 🏎️' },
+        { types: ['security'], label: 'Security 👮' },
+        { types: ['refactor'], label: 'Refactoring 🛠' },
+        { types: ['test'], label: 'Automated Tests ✅' },
+        { types: ['infra'], label: 'Infrastructure 🏗️' },
+        { types: ['ci'], label: 'Continuous Integration/Deployment 🚢' },
+        { types: ['docs'], label: 'Documentation 📚' },
+        { types: ['cs'], label: 'Codestyle 🧹' },
+        { types: ['misc'], label: 'Miscelanneous ❓' },
+        { types: ['revert'], label: 'Revert ⏪' }
+    ],
+};
