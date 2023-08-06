e8c7874fc7f97b4c99b2baa40fc20c122c50ffe4
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-08-06 16:57:10 +0200

    Add Symfony profiler

--- backend/config/packages/dev/framework.php
+++ backend/config/packages/dev/framework.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 use Symfony\Config\FrameworkConfig;
 
 return static function (FrameworkConfig $config): void {
-    $config->test(true);
-
-    $config->session()
-        ->storageFactoryId('session.storage.factory.mock_file');
-
     $config->profiler()
-        ->collect(false);
+        ->onlyExceptions(false)
+        ->collectSerializerData(true);
 };
