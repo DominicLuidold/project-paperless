b184ede1f47680040ef851d73feaeaba0f51b4d4
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-08-06 12:02:33 +0200

    Add symfony/validator

--- backend/config/packages/validator.php
+++ backend/config/packages/validator.php
@@ -5,8 +5,5 @@ declare(strict_types=1);
 use Symfony\Config\FrameworkConfig;
 
 return static function (FrameworkConfig $config): void {
-    $cache = $config->cache();
-
-    $cache->app('cache.adapter.redis')
-        ->defaultRedisProvider('%env(REDIS_DSN)%');
+    $config->validation()->emailValidationMode('html5');
 };
