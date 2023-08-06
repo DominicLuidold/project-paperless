fc082a2a7b8a45eccd822c62275147b43b368276
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-08-07 00:12:07 +0200

    Add test for NelmioApiDoc page

--- backend/config/routes/test/nelmio_api_doc.php
+++ backend/config/routes/test/nelmio_api_doc.php
@@ -5,10 +5,6 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes): void {
-    $routes->add('app.swagger', '/api/doc.json')
-        ->methods(['GET'])
-        ->controller('nelmio_api_doc.controller.swagger');
-
     $routes->add('app.swagger_ui', '/api/doc')
         ->methods(['GET'])
         ->controller('nelmio_api_doc.controller.swagger_ui');
--- backend/rector.php
+++ backend/rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
@@ -34,5 +35,9 @@ return static function (RectorConfig $rectorConfig): void {
         PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
         PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+    ]);
+
+    $rectorConfig->skip([
+        PreferPHPUnitThisCallRector::class,
     ]);
 };
