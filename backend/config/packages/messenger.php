b48de9e5c7edc37f9bf4538845eb6c885b30f1dd
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-08-06 11:11:15 +0200

    Add Messenger

---
+++ backend/config/packages/messenger.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Config\FrameworkConfig;
+
+return static function (FrameworkConfig $framework): void {
+    $messenger = $framework->messenger();
+
+    $messenger->defaultBus('event.bus')
+        ->failureTransport('failed');
+
+    $messenger->transport('sync')
+        ->dsn('sync://');
+
+    $messenger->transport('async')
+        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
+        ->options(['queue_name' => 'async', 'auto_setup' => false])
+        ->retryStrategy()
+        ->maxRetries(3)
+        ->delay(1000)
+        ->maxDelay(0)
+        ->multiplier(10);
+
+    $messenger->transport('failed')
+        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
+        ->options(['queue_name' => 'failed', 'auto_setup' => false]);
+
+    $commandBus = $messenger->bus('command.bus');
+    $commandBus->middleware()->id('dispatch_after_current_bus');
+    $commandBus->middleware()->id('doctrine_transaction');
+
+    $queryBus = $messenger->bus('query.bus');
+    $queryBus->middleware()->id('dispatch_after_current_bus');
+
+    $eventBus = $messenger->bus('event.bus');
+    $eventBus->middleware()->id('dispatch_after_current_bus');
+    $eventBus->middleware()->id('doctrine_transaction');
+};
--- backend/config/services.php
+++ backend/config/services.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Framework\Application\Messenger\CommandHandlerInterface;
+use Framework\Application\Messenger\EventHandlerInterface;
+use Framework\Application\Messenger\QueryHandlerInterface;
+
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
 
@@ -11,5 +15,15 @@ return static function (ContainerConfigurator $container): void {
         ->autowire()
         ->autoconfigure();
 
+    $services->instanceof(CommandHandlerInterface::class)
+        ->tag('messenger.message_handler', ['bus' => 'command.bus']);
+
+    $services->instanceof(QueryHandlerInterface::class)
+        ->tag('messenger.message_handler', ['bus' => 'query.bus']);
+
+    $services->instanceof(EventHandlerInterface::class)
+        ->tag('messenger.message_handler', ['bus' => 'event.bus']);
+
     $services->load('App\\', '../src/App/*');
+    $services->load('Framework\\', '../src/Framework/*');
 };
