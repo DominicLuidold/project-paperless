f3d4649024ee184626e56bb1f6c1af84bba39509
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-12-30 11:25:31 +0100

    Improve frontend setup

--- docker/frontend/node-ci.sh
+++ docker/frontend/node-ci.sh
@@ -1,22 +1,16 @@
 #!/bin/bash -e
 
-source .env
-
-if [ -e .env.local ]; then
-    source .env.local
-fi
-
 docker build \
-    -t ${PROJECT_NAME}-node-local \
-    --build-arg NODE_VERSION=${NODE_VERSION} \
+    -t "${PROJECT_NAME}"-node-local \
+    --build-arg NODE_VERSION="${NODE_VERSION}" \
     ./docker/frontend/.
 
 docker run --rm -it \
     --publish 4200:4200 \
-    --volume ${PWD}/frontend:/usr/src/app \
+    --volume "${PWD}"/frontend:/usr/src/app \
     --volume /dev/shm:/dev/shm \
     --workdir /usr/src/app \
     --network "${PROJECT_NAME}_default" \
     --privileged \
     --name "${PROJECT_NAME}-frontend" \
-    ${PROJECT_NAME}-node-local /bin/bash
+    "${PROJECT_NAME}"-node-local /bin/bash
