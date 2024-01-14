5ffd7fb17c1f782474700e6a4cbfa140dffd8306
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2024-01-14 17:32:53 +0100

    WIP: Sidenav, toolbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,31 +1,33 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { map, shareReplay } from 'rxjs/operators';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @Component({
-    imports: [
-        AsyncPipe,
-        CommonModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        RouterLink,
-        RouterOutlet,
-    ],
     selector: 'app-root',
     standalone: true,
     styleUrl: './app.component.scss',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    imports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatListModule,
+        MatIconModule,
+        AsyncPipe,
+        RouterLink,
+        RouterLinkActive,
+        MatTooltipModule,
+        RouterOutlet,
+    ]
 })
 export class AppComponent {
     private breakpointObserver = inject(BreakpointObserver);
